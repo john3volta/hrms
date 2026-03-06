65300bcc890af0a6c70d17baf1947deadae436d3
Asmita Hase <asmitahase@gmail.com>
2026-03-06 22:26:12 +0530

    refactor(tests): test employee onboarding

--- hrms/hr/doctype/employee_onboarding/test_employee_onboarding.py
+++ hrms/hr/doctype/employee_onboarding/test_employee_onboarding.py
@@ -17,10 +17,10 @@ from hrms.tests.utils import HRMSTestSuite
 class TestEmployeeOnboarding(HRMSTestSuite):
 	def setUp(self):
 		create_company()
-		if frappe.db.exists("Employee Onboarding", {"employee_name": "Test Researcher"}):
-			frappe.db.sql("delete from `tabEmployee Onboarding` where employee_name=%s", "Test Researcher")
+		if frappe.db.exists("Employee Onboarding", {"employee_name": "Test Engineer"}):
+			frappe.db.sql("delete from `tabEmployee Onboarding` where employee_name=%s", "Test Engineer")
 
-		project = "Employee Onboarding : test@researcher.com"
+		project = "Employee Onboarding : test@engineer.com"
 		frappe.db.sql("delete from tabProject where project_name=%s", project)
 		frappe.db.sql("delete from tabTask where project=%s", project)
 
@@ -28,7 +28,7 @@ class TestEmployeeOnboarding(HRMSTestSuite):
 		onboarding = create_employee_onboarding()
 
 		project_name = frappe.db.get_value("Project", onboarding.project, "project_name")
-		self.assertEqual(project_name, "Employee Onboarding : test@researcher.com")
+		self.assertEqual(project_name, "Employee Onboarding : test@engineer.com")
 
 		# don't allow making employee if onboarding is not complete
 		self.assertRaises(IncompleteTaskError, make_employee, onboarding.name)
@@ -66,7 +66,7 @@ class TestEmployeeOnboarding(HRMSTestSuite):
 		employee.date_of_birth = "1990-05-08"
 		employee.gender = "Female"
 		employee.insert()
-		self.assertEqual(employee.employee_name, "Test Researcher")
+		self.assertEqual(employee.employee_name, "Test Engineer")
 
 	def test_mark_onboarding_as_completed(self):
 		onboarding = create_employee_onboarding()
@@ -90,14 +90,14 @@ class TestEmployeeOnboarding(HRMSTestSuite):
 
 
 def get_job_applicant():
-	if frappe.db.exists("Job Applicant", "test@researcher.com"):
-		return frappe.get_doc("Job Applicant", "test@researcher.com")
+	if frappe.db.exists("Job Applicant", "test@engineer.com"):
+		return frappe.get_doc("Job Applicant", "test@engineer.com")
 	applicant = frappe.new_doc("Job Applicant")
-	applicant.applicant_name = "Test Researcher"
-	applicant.email_id = "test@researcher.com"
-	applicant.designation = "Researcher"
+	applicant.applicant_name = "Test Engineer"
+	applicant.email_id = "test@engineer.com"
+	applicant.designation = "Engineer"
 	applicant.status = "Open"
-	applicant.cover_letter = "I am a great Researcher."
+	applicant.cover_letter = "I am a great Engineer."
 	applicant.insert()
 	return applicant
 
@@ -127,7 +127,7 @@ def create_employee_onboarding():
 	onboarding.date_of_joining = onboarding.boarding_begins_on = getdate()
 	onboarding.company = "_Test Company"
 	onboarding.holiday_list = holiday_list.name
-	onboarding.designation = "Researcher"
+	onboarding.designation = "Engineer"
 	onboarding.append(
 		"activities",
 		{

bd0bc5adcc6597b5d75bef3171610417c93f7f1a
ruthra kumar <ruthra@erpnext.com>
2026-03-08 06:49:39 +0530

    refactor(test): make leave policy assignment deterministic

--- hrms/hr/doctype/leave_policy_assignment/test_leave_policy_assignment.py
+++ hrms/hr/doctype/leave_policy_assignment/test_leave_policy_assignment.py
@@ -17,16 +17,6 @@ from hrms.tests.utils import HRMSTestSuite
 
 class TestLeavePolicyAssignment(HRMSTestSuite):
 	def setUp(self):
-		for doctype in [
-			"Leave Period",
-			"Leave Application",
-			"Leave Allocation",
-			"Leave Policy",
-			"Leave Policy Assignment",
-			"Leave Ledger Entry",
-		]:
-			frappe.db.delete(doctype)
-
 		employee = get_employee()
 		self.original_doj = employee.date_of_joining
 		self.employee = employee
@@ -150,7 +140,7 @@ class TestLeavePolicyAssignment(HRMSTestSuite):
 		)
 		self.assertEqual(new_leaves_allocated, 30)
 
-		leave_period = create_leave_period(add_months(first_day, -23), first_day)
+		leave_period = create_leave_period(add_months(first_day, -23), first_day, "_Test Company")
 		data = {
 			"assignment_based_on": "Leave Period",
 			"leave_policy": leave_policy.name,
@@ -209,7 +199,7 @@ class TestLeavePolicyAssignment(HRMSTestSuite):
 	def test_earned_leave_allocation_if_leave_policy_assignment_submitted_after_period(self):
 		year_start_date = get_year_start(getdate())
 		year_end_date = get_year_ending(getdate())
-		leave_period = create_leave_period(year_start_date, year_end_date)
+		leave_period = create_leave_period(year_start_date, year_end_date, "_Test Company")
 
 		# assignment 10 days after the leave period
 		frappe.flags.current_date = add_days(year_end_date, 10)
@@ -236,7 +226,7 @@ class TestLeavePolicyAssignment(HRMSTestSuite):
 	def test_earned_leave_allocation_for_leave_period_spanning_two_years(self):
 		first_year_start_date = get_year_start(getdate())
 		second_year_end_date = get_year_ending(add_months(first_year_start_date, 12))
-		leave_period = create_leave_period(first_year_start_date, second_year_end_date)
+		leave_period = create_leave_period(first_year_start_date, second_year_end_date, "_Test Company")
 
 		# assignment during mid second year
 		frappe.flags.current_date = add_months(second_year_end_date, -6)
@@ -264,7 +254,7 @@ class TestLeavePolicyAssignment(HRMSTestSuite):
 
 	def test_skip_zero_allocation_leaves(self):
 		today = getdate()
-		leave_period = create_leave_period(get_year_start(today), get_year_ending(today))
+		leave_period = create_leave_period(get_year_start(today), get_year_ending(today), "_Test Company")
 
 		sick = create_leave_type(
 			leave_type_name="_Test Sick Leave", non_encashable_leaves=0, max_leaves_allowed=2

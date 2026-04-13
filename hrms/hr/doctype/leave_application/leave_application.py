ff03722c54c44edab27318bb2d3b6efccf7f2cf8
Krishna Shirsath <shirsathkrishna19@gmail.com>
2026-04-13 16:27:27 +0530

    fix: remove merge conflict

--- hrms/hr/doctype/leave_application/leave_application.py
+++ hrms/hr/doctype/leave_application/leave_application.py
@@ -937,13 +937,9 @@ def get_number_of_leave_days(
 
 
 @frappe.whitelist()
-<<<<<<< HEAD
-def get_leave_details(employee, date, for_salary_slip=False):
-=======
 def get_leave_details(employee: str, date: str | datetime.date, for_salary_slip: bool = False) -> dict:
 	frappe.has_permission("Employee", "read", employee, throw=True)
 
->>>>>>> 7cf3240e (fix: add permission checks for employee leave detail)
 	allocation_records = get_leave_allocation_records(employee, date)
 	leave_allocation = {}
 	precision = cint(frappe.db.get_single_value("System Settings", "float_precision")) or 2
